143c5b6a3c0400075e9f2206df0541afbca52ca7
ciooo <102889721+habieell@users.noreply.github.com>
2025-04-24 14:54:41 +0700

    changes title website

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -9,8 +9,7 @@ import RecentOrders from "@/components/dashboard/RecentOrders";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "Arraya | Admin Dashboard",
 };
 
 export default function Ecommerce() {
--- src/components/header/NotificationDropdown.tsx
+++ src/components/header/NotificationDropdown.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import React, { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
@@ -372,12 +371,7 @@ export default function NotificationDropdown() {
           </li>
           {/* Add more items as needed */}
         </ul>
-        <Link
-          href="/"
-          className="block px-4 py-2 mt-3 text-sm font-medium text-center text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
-        >
-          View All Notifications
-        </Link>
+        
       </Dropdown>
     </div>
   );
