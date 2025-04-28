faea7b4d3aece24e77c192045536e4420a19a08b
ciooo <102889721+habieell@users.noreply.github.com>
2025-04-28 17:19:35 +0700

    perizinantable.tsx fix

--- src/components/tables/PerizinanTable.tsx
+++ src/components/tables/PerizinanTable.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
 import Cookies from "js-cookie";
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "../ui/table";
 import PerizinanDetailModal from "../example/ModalExample/PerizinanDetailModal";
