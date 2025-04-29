047a92ec9a5ad6c5f4bdf46b41f169658353e840
ciooo <102889721+habieell@users.noreply.github.com>
2025-04-29 15:07:55 +0700

    deleted layout title

--- src/app/(admin)/(others-pages)/(tables)/tabel-perizinan/page.tsx
+++ src/app/(admin)/(others-pages)/(tables)/tabel-perizinan/page.tsx
@@ -1,14 +1,7 @@
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadCrumb from "@/components/common/PageBreadCrumb";
-import { Metadata } from "next";
 import React from "react";
 import PerizinanTable from "@/components/tables/PerizinanTable";
-
-export const metadata: Metadata = {
-  title: "Next.js Basic Table | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Basic Table page for TailAdmin Tailwind CSS Admin Dashboard Template",
-};
 
 export default function BasicTables() {
   return (
